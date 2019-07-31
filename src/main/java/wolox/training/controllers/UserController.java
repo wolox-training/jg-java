f4676b08b66d473ea563191ba4b733cedf946d0f
jcgrethe <jgrethe@itba.edu.ar>
2019-07-31 18:19:59 -0300

    get current user

--- src/main/java/wolox/training/controllers/UserController.java
+++ src/main/java/wolox/training/controllers/UserController.java
@@ -2,6 +2,7 @@ package wolox.training.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import wolox.training.exceptions.BookAlreadyOwnedException;
@@ -68,6 +71,12 @@ public class UserController {
         userRepository.findById(id)
             .orElseThrow(UserNotFoundException::new);
         return userRepository.save(user);
+    }
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public User currentUserName(Authentication authentication) {
+        return userRepository.findByUsername(authentication.getName()).get();
     }
 
     @PutMapping("/{id}/books/{book_id}")
